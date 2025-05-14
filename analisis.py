8e4f4216a948d21ed69a9f6bebfc397f48110ed8
“[natali <natali.velandia@gmail.com>
2025-05-14 16:38:38 -0500

    Agregamos tkinter a la visualizacion

--- analisis.py
+++ analisis.py
@@ -5,14 +5,21 @@ import matplotlib.pyplot as plt
 # Seaborn para visualizaciones más estilizadas
 import seaborn as sns
 import os
+import io 
 
 class DataAnalyzer:
   def __init__(self, data):
     self.df = data
 
   def summary(self):
-    print(self.df.info())
-    print(self.df.describe())
+    buffer = io.StringIO()
+    self.df.info(buf= buffer)
+    salida = buffer.getvalue()
+    salida_describe = self.df.describe().to_string()
+    salida += "\n\n" + salida_describe
+    return salida
+
+
   def missing_values(self):
     return self.df.isnull()
   def imprimir(self):
--- visualizacion.py
+++ visualizacion.py
@@ -6,16 +6,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 from analisis import DataAnalyzer
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+from tkinter import messagebox
 
 data = pd.read_csv("adult.csv")
 analizar = DataAnalyzer(data)
-analizar.summary()
-analizar.correlation_matrix()
-analizar.categorical_analisis()
+
+def informacion():
+    try:
+        text_area.delete("1.0",tk.END) #Para vaciar al ejecutar
+        info = analizar.summary()
+        text_area.insert(tk.END, info)
+    except:
+        messagebox.showerror("Error", "No se puede")
 
 
-data = pd.read_csv("vehicles.csv")
-analizar = DataAnalyzer(data)
-analizar.summary()
-analizar.correlation_matrix()
-analizar.categorical_analisis()
+ventana = tk.Tk()
+ventana.title("Análisis de datos")
+
+boton_summary = tk.Button(ventana, text="Info", command=informacion)
+boton_summary.pack()
+
+text_area = ScrolledText(ventana, width= 70, 
+                                         height=30)
+text_area.pack()
+
+ventana.mainloop()
