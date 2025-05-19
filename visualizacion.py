f8d20eb74f94395b858cb7c38b71ee71f011b56b
“[natali <natali.velandia@gmail.com>
2025-05-19 15:44:03 -0500

    Cambio visualizacion

--- visualizacion.py
+++ visualizacion.py
@@ -8,7 +8,8 @@ import os
 from analisis import DataAnalyzer
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
-from tkinter import messagebox
+from tkinter import messagebox, simpledialog, filedialog
+from PIL import ImageTk
 
 data = pd.read_csv("adult.csv")
 analizar = DataAnalyzer(data)
@@ -21,15 +22,42 @@ def informacion():
     except:
         messagebox.showerror("Error", "No se puede")
 
+def mostrar_imagenes(pil_img):
+    image_tk = ImageTk.PhotoImage(pil_img)
+    image_label.configure(image=image_tk)
+    image_label.image = image_tk
+
+def mostrar_correlacion():
+    img = analizar.correlation_matrix()
+    mostrar_imagenes(img)
+
+def mostrar_categorico():
+    cols = analizar.df.select_dtypes(include="object").columns.tolist()
+    if not cols:
+        messagebox.showwarning("Atención", "El df no tiene col. categoricas")
+    else:
+        sel = simpledialog.askstring("Columna", f"Elige una:\n {cols}")
+        if sel in cols:
+            img = analizar.categorical_analisis_col(sel)
+            mostrar_imagenes(img)
 
 ventana = tk.Tk()
 ventana.title("Análisis de datos")
 
-boton_summary = tk.Button(ventana, text="Info", command=informacion)
-boton_summary.pack()
+boton_summary = tk.Button(ventana, text="Resumen", command=informacion)
+boton_summary.grid(row=0, column=0)
 
-text_area = ScrolledText(ventana, width= 70, 
-                                         height=30)
-text_area.pack()
+boton_numerico = tk.Button(ventana, text="Numerico", command=mostrar_correlacion)
+boton_numerico.grid(row=0, column=1)
 
+boton_categorico = tk.Button(ventana, text="Categórico", command=mostrar_categorico)
+boton_categorico.grid(row=0, column=2)
+
+text_area = ScrolledText(ventana, width= 70, height=30)
+text_area.grid(row=1, column=1)
+
+content_frame = tk.Frame(ventana)
+content_frame.grid(row=1, column=2)
+image_label = tk.Label(content_frame)
+image_label.grid(row=0,column=0)
 ventana.mainloop()
